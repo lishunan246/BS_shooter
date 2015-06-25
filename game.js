acfc57442da96f41ded7d1e4fe4e65cac3c5775c
李书楠 <lishunan246@gmail.com>
2015-06-25 23:08:55 +0800

    refactor

--- game.js
+++ game.js
@@ -2,6 +2,18 @@
 var socket = io.connect('http://localhost:8000');
 var uid;
 
+//my position
+var me = {x:240,y:750};
+
+socket.on("your move",function(data){
+    me.x=data.x;
+    me.y=data.y;
+});
+
+socket.on('broadcast',function(data){
+   console.log(data);
+});
+
 socket.on('getUid', function (data) {
     console.log(data);
     uid=data.id;
@@ -28,19 +40,19 @@ var img = newImg("./assets/bg_01.jpg");
 var fps;
 cxt.drawImage(img,0,0,480,800);
 
-var flivverLog = 0;
-var flivver1 = newImg("./assets/flivver.png");
-var flivver2 = newImg("./assets/flivver2.png");
-var flivver3 = newImg("./assets/flivver3.png");
+var planeLog = 0;
+var planeImg1 = newImg("./assets/flivver.png");
+var planeImg2 = newImg("./assets/flivver2.png");
+var planeImg3 = newImg("./assets/flivver3.png");
 
 // 用于记录游戏的时间，越到后面越快
 var time1 = 0;
 var time2 = 80;
 
 // 积分
-var jifen = 0;
+var playerScore = 0;
 
-function getSudu(){
+function getSpeed(){
 	var number = parseInt(Math.random()*10);
 	if(number < 5 && number > 0){
 		return number;
@@ -48,7 +60,7 @@ function getSudu(){
 	return 1;
 }
 // 飞机的对象
-function FlivverObj(hp,ewidth,eheight,eimg,esudu){
+function Plane(hp, width, height, img, speed){
 	// 随机的X
 	this.x = parseInt(Math.random()*460+1);
 	this.y = 0;
@@ -59,21 +71,21 @@ function FlivverObj(hp,ewidth,eheight,eimg,esudu){
 	// 是否死亡
 	this.over = 0;
 	
-	this.width = ewidth;
-	this.height = eheight;
-	this.img = eimg;
-	this.sudu = esudu;
+	this.width = width;
+	this.height = height;
+	this.img = img;
+	this.speed = speed;
 }
 
 // 获取飞机
-function getFlivver(type){
+function getPlaneByType(type){
 	switch(type){
 		case 1:
-			return new FlivverObj(100,50,30,flivver1,getSudu());
+			return new Plane(100, 50, 30, planeImg1, getSpeed());
 		case 2:
-			return new FlivverObj(500,70,90,flivver2,getSudu());
+			return new Plane(500, 70, 90, planeImg2, getSpeed());
 		case 3:
-			return new FlivverObj(1000,110,170,flivver3,getSudu());
+			return new Plane(1000, 110, 170, planeImg3, getSpeed());
 	}
 }
 function Cartridge(x,y){
@@ -90,7 +102,7 @@ function gameover(){
     {
 		$(this).html('你屎').hide().fadeIn(1000,function()
         {
-			$(this).html('<a href="javascript:location.reload();" style="color:#cc0000" title="重新开始">你屎了</a><br>' + jifen  + ' 分').hide().fadeIn();
+			$(this).html('<a href="javascript:location.reload();" style="color:#cc0000" title="重新开始">你屎了</a><br>' + playerScore  + ' 分').hide().fadeIn();
 		});	
 	});
 }
@@ -98,16 +110,16 @@ function gameover(){
 (
 	function(cxt)
     {
-        var planes = {nums:0};
+        var planes = {planeCount:0};
         // 用于存放小飞机
-        var flivver = [];
+        var planeArray = [];
 
         // 自己
-        var me = {x:240,y:750};
-        var meImg = newImg('assets/me.png');
+
+        var meImg = newImg('assets/me.png');//64*72
         // 子弹
-        var cartridges = [];
-        var cartridgeImg = newImg('./assets/cartridge.png');
+        var bulletArray = [];
+        var bulletImg = newImg('./assets/cartridge.png');
 
         var boo1 = newImg('./assets/boo1.png');
         var over = newImg('./assets/over.png');
@@ -127,10 +139,10 @@ function gameover(){
 
 
             cxt.font = "italic 20px 微软雅黑";
-            cxt.strokeText("积分：" + jifen, 10, 30);
+            cxt.strokeText("积分：" + playerScore, 10, 30);
 
-            $('#fjs').html(flivver.length);
-            $('#zds').html(cartridges.length);
+            $('#fjs').html(planeArray.length);
+            $('#zds').html(bulletArray.length);
             $('#scfj').html("1000/" + time2 + " 毫秒");
         };
 
@@ -149,66 +161,66 @@ function gameover(){
          * 设置移动的背景
          */
         planes.setBg = function(){
-            planes.nums++;
-            if(planes.nums == 800){
-                planes.nums = 0;
+            planes.planeCount++;
+            if(planes.planeCount == 800){
+                planes.planeCount = 0;
             }
             // 画布的背景
-            cxt.drawImage(img,0,planes.nums,480,800);
-            cxt.drawImage(img,0,planes.nums - 800,480,800);
+            cxt.drawImage(img,0,planes.planeCount,480,800);
+            cxt.drawImage(img,0,planes.planeCount - 800,480,800);
         };
 
         planes.setFlivver = function(){
             // 生成飞机
-            if(planes.nums % time2 == 0){
-                flivverLog++;
-                if(flivverLog % 6 == 0){
-                    flivver.push(getFlivver(2));
-                }else if(flivverLog % 13 == 0){
-                    flivver.push(getFlivver(3));
+            if(planes.planeCount % time2 == 0){
+                planeLog++;
+                if(planeLog % 6 == 0){
+                    planeArray.push(getPlaneByType(2));
+                }else if(planeLog % 13 == 0){
+                    planeArray.push(getPlaneByType(3));
                 }else{
-                    flivver.push(getFlivver(1));
+                    planeArray.push(getPlaneByType(1));
                 }
 
             }
 
-            for(var a in flivver){
+            for(var a in planeArray){
 
 
 
-                flivver[a].y += flivver[a].sudu;
+                planeArray[a].y += planeArray[a].speed;
                 // 如果超出屏幕将该小飞机删除
-                if(flivver[a].y > 780){
-                    flivver.splice(a, 1);
+                if(planeArray[a].y > 780){
+                    planeArray.splice(a, 1);
                 }
                 // 将小飞机画到画布上
 
 
                 // 小飞机死亡
-                if(flivver[a].over > 0){
-                    flivver[a].over --;
+                if(planeArray[a].over > 0){
+                    planeArray[a].over --;
 
-                    if(flivver[a].over > 20){
-                        cxt.drawImage(boo1,flivver[a].x + flivver[a].width/2 - 20  ,flivver[a].y + flivver[a].height / 2 -10,41,39);
-                    }else if(flivver[a].over > 2){
-                        cxt.drawImage(over,flivver[a].x + flivver[a].width/2 - 20 ,flivver[a].y + flivver[a].height / 2 -10,40,43);
+                    if(planeArray[a].over > 20){
+                        cxt.drawImage(boo1,planeArray[a].x + planeArray[a].width/2 - 20  ,planeArray[a].y + planeArray[a].height / 2 -10,41,39);
+                    }else if(planeArray[a].over > 2){
+                        cxt.drawImage(over,planeArray[a].x + planeArray[a].width/2 - 20 ,planeArray[a].y + planeArray[a].height / 2 -10,40,43);
                     }else{
-                        flivver.splice(a, 1);
+                        planeArray.splice(a, 1);
                     }
 
 
 
                 }else{
-                    cxt.drawImage(flivver[a].img,flivver[a].x,flivver[a].y,flivver[a].width,flivver[a].height);
+                    cxt.drawImage(planeArray[a].img,planeArray[a].x,planeArray[a].y,planeArray[a].width,planeArray[a].height);
                     // 判断自己是否死亡
-                    if( me.x > (flivver[a].x - flivver[a].width + 20) && (me.x) <(flivver[a].x + flivver[a].width - 20) && (me.y) < (flivver[a].y + flivver[a].height + 20) && (me.y + 72) > (flivver[a].y - 20)){
+                    if( me.x > (planeArray[a].x - planeArray[a].width + 20) && (me.x) <(planeArray[a].x + planeArray[a].width - 20) && (me.y) < (planeArray[a].y + planeArray[a].height + 20) && (me.y + 72) > (planeArray[a].y - 20)){
                         gameover();
                     }
 
-                    if(flivver[a].hit > 0){
-                        cxt.drawImage(boo1,flivver[a].x + flivver[a].width/2 - 20 ,flivver[a].y + flivver[a].height / 2 -10,41,39);
+                    if(planeArray[a].hit > 0){
+                        cxt.drawImage(boo1,planeArray[a].x + planeArray[a].width/2 - 20 ,planeArray[a].y + planeArray[a].height / 2 -10,41,39);
                         //cxt.drawImage(boo1,flivver[a].x + 5 ,flivver[a].y,41,39);
-                        flivver[a].hit--;
+                        planeArray[a].hit--;
                     }
                 }
 
@@ -217,45 +229,45 @@ function gameover(){
 
         // 更新自己的距离
         planes.setMe = function(){
-            cxt.drawImage(meImg,me.x,me.y,64,72);
+            cxt.drawImage(meImg,me.x-32,me.y-36,64,72);
         };
 
         // 更新子弹方法
         planes.cartridge = function(){
-            if(planes.nums % 10 == 0){
-                cartridges.push(new Cartridge(me.x + 30,me.y));
+            if(planes.planeCount % 10 == 0){
+                bulletArray.push(new Cartridge(me.x,me.y));
             }
 
-            for(var i in cartridges){
+            for(var i in bulletArray){
                 // 飞到顶部就将OBJ删除掉
-                if(cartridges[i].y < 0){
-                    cartridges.splice(i, 1);
+                if(bulletArray[i].y < 0){
+                    bulletArray.splice(i, 1);
                     continue;
                 }
 
 
-                cartridges[i].y -= 20;
+                bulletArray[i].y -= 20;
                 // 将小飞机画到画布上
-                cxt.drawImage(cartridgeImg,cartridges[i].x,cartridges[i].y,7,17);
+                cxt.drawImage(bulletImg,bulletArray[i].x,bulletArray[i].y,7,17);
 
                 // 子弹碰到飞机的情况
-                for(var j in flivver){
-                    if(flivver[j].over > 0){
+                for(var j in planeArray){
+                    if(planeArray[j].over > 0){
                         continue;
                     }
-                    if(cartridges[i].x > flivver[j].x && cartridges[i].x < flivver[j].x+ flivver[j].width && cartridges[i].y >  flivver[j].y && cartridges[i].y -flivver[j].height < flivver[j].y){
+                    if(bulletArray[i].x > planeArray[j].x && bulletArray[i].x < planeArray[j].x+ planeArray[j].width && bulletArray[i].y >  planeArray[j].y && bulletArray[i].y -planeArray[j].height < planeArray[j].y){
 
-                        flivver[j].hit = 10;
+                        planeArray[j].hit = 10;
                         $('#isdz').html('打中了编号' + j);
 
-                        if(flivver[j].hp > 1){
-                            flivver[j].hp -= 80;
+                        if(planeArray[j].hp > 1){
+                            planeArray[j].hp -= 80;
                         }else{
-                            flivver[j].over = 40;
-                            jifen += 50000;
+                            planeArray[j].over = 40;
+                            playerScore += 1;
                         }
                         // 子弹消失
-                        cartridges.splice(i, 1);
+                        bulletArray.splice(i, 1);
                         break;
                     }
                 }
@@ -264,9 +276,9 @@ function gameover(){
 
         // 绑定鼠标事件
         c.addEventListener('mousemove', function onMouseMove(evt) {
-            me.x=evt.layerX;
-            //me.x = evt.layerX - $('#canvas').offset().left - 32;
-            me.y = evt.layerY -  36 ;
+            //me.x=evt.layerX;
+            ////me.x = evt.layerX - $('#canvas').offset().left - 32;
+            //me.y = evt.layerY ;
             $('#sbX').html(me.x);
             $('#sbY').html(me.y);
             if(uid==null)
@@ -274,7 +286,7 @@ function gameover(){
                 console.log("no uid");
             }
             else{
-                socket.emit("move",{x:me.x,y:me.y,uid:uid});
+                socket.emit("move",{x:evt.layerX,y:evt.layerY,uid:uid});
             }
         });
 
@@ -289,4 +301,4 @@ function newImg(src){
 	return obj;
 }
 
-//setInterval(h.update, 1000/65); 
+//setInterval(h.update, 1000/65);
--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 var io = require('socket.io')(8000);
 
 var uid=0;
+var userCount=0;
+
+var planes={
+    planeCount:0
+};
+
+planes.update=function()
+{
+
+};
+
+function update()
+{
+    io.sockets.emit('broadcast',
+        {
+            userCount:userCount
+        });
+}
+
+setInterval(update,1000);
+
 
 
 io.on('connection', function (socket) {
@@ -12,6 +33,7 @@ io.on('connection', function (socket) {
     else
     {
         uid++;
+        userCount++;
         socket.uid=uid;
         socket.emit('getUid', { id: socket.uid });
         socket.broadcast.emit("add player",{id:socket.uid});
@@ -21,14 +43,16 @@ io.on('connection', function (socket) {
     //ÍË³ö
     socket.on("disconnect",function()
     {
+        userCount--;
         socket.broadcast.emit("remove player",{ id: socket.uid });
-       console.log(socket.uid+ " disconnected.");
+        console.log(socket.uid+ " disconnected.");
     });
 
     //ÒÆ¶¯
     socket.on("move",function(data)
     {
-       console.log(data);
+        console.log(data);
+        socket.emit("your move",data);
         socket.broadcast.emit("p2 move",data);
     });
 });
